24bb8e10e0eb2f298a2475717434e69714c85dee
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2015-12-22 09:10:13 +0100

    Fixed sequence download

---
+++ newAppTest/src/main/java/server/MyFloatTypeAdapter.java
@@ -0,0 +1,36 @@
+package server;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+public class MyFloatTypeAdapter extends TypeAdapter<Float>{
+	
+	@Override
+    public Float read(JsonReader reader) throws IOException {
+        if (reader.peek() == JsonToken.NULL) {
+            reader.nextNull();
+            return new Float(0);
+        }
+        String stringValue = reader.nextString();
+        try {
+            Float value = Float.valueOf(stringValue);
+            return value;
+        } catch (NumberFormatException e) {
+            return new Float(0);
+        }
+    }
+
+    @Override
+    public void write(JsonWriter writer, Float value) throws IOException {
+        if (value == null) {
+            writer.value(0);
+            return;
+        }
+        writer.value(value);
+    }
+
+}
