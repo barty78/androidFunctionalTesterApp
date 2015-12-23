92e8bb6dafbea75281a6f26a14df4e9d3ba17a57
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2015-12-23 09:17:42 +0100

    W£orking...

--- newAppTest/src/main/java/server/service/ServiceDBHelper.java
+++ newAppTest/src/main/java/server/service/ServiceDBHelper.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import com.activeandroid.ActiveAndroid;
+import com.activeandroid.Model;
 import com.activeandroid.query.Select;
 
 import server.pojos.Device;
@@ -11,16 +12,26 @@ import server.pojos.Device;
 public class ServiceDBHelper {
 	
 	public static void addDevices(List<Device> arg0){
-		HashSet<Device> devices= new HashSet<Device>();
-		devices.addAll(arg0);
-		HashSet<Device> temp= new HashSet<Device>();
-		for(Device device:devices){
-			if(!isDeviceAlreadySeen(device))temp.add(device);
+		HashSet<Device> devicesReceived= new HashSet<Device>();
+		HashSet<Device> devicesToBeSaved= new HashSet<Device>();
+		devicesReceived.addAll(arg0);
+		for (Device device:devicesReceived) {
+			Device existingdevice=null;
+			existingdevice=weHaveItAlready(device.getBarcode());
+			if(existingdevice==null)devicesToBeSaved.add(device);
+			else {
+				if(device.getBt_addr()!=null && device.getBt_addr().length()<=0)existingdevice.setBt_addr(device.getBt_addr());
+				if(device.getDeviceId()!=0 )existingdevice.setDeviceId(device.getDeviceId());
+				if(device.getFwver()!=null && device.getFwver().length()<=0)existingdevice.setFwver(device.getFwver());
+				if(device.getJobId()!=0 )existingdevice.setJobId(device.getJobId());
+				if(device.getModel()!=null && device.getModel().length()<=0)existingdevice.setModel(device.getModel());
+				if(device.getSerial()!=null && device.getSerial().length()<=0)existingdevice.setSerial(device.getSerial());
+				devicesToBeSaved.add(existingdevice);
+			}
 		}
-		
 		ActiveAndroid.beginTransaction();
 		try {
-		        for (Device device:temp) {
+		        for (Device device:devicesToBeSaved) {
 		        	device.save();
 		        }
 		        ActiveAndroid.setTransactionSuccessful();
@@ -29,7 +40,13 @@ public class ServiceDBHelper {
 		        ActiveAndroid.endTransaction();
 		}
 	}
-	
+
+	private static Device weHaveItAlready(String barcode) {
+		Device device=null;
+		device= new Select().from(Device.class).where("Barcode = ?",barcode).executeSingle();
+		return device;
+	}
+
 	private static boolean isDeviceAlreadySeen(Device device){
 		List sameBarcode= new Select().from(Device.class).where("Barcode = ?",device.getBarcode()).execute();
 		if(sameBarcode!=null && sameBarcode.size()>0)return true;
@@ -49,4 +66,43 @@ public class ServiceDBHelper {
 		if(sameSerial!=null && sameSerial.size()>0)return true;
 		return false;
 		}
+
+	public static Long saveBarcode(String barcode){
+		if(barcode==null || barcode.length()<=0)return -1l;
+		Device device= new Device();
+		device.setBarcode(barcode);
+		return device.save();
+	}
+	public static Long saveSerial(String barcode, String serial){
+		if(barcode==null || barcode.length()<=0)return -1l;
+		if(serial==null)return -1l;
+		Device device = new Select().from(Device.class).where("Barcode = ?", barcode).executeSingle();
+		device.setSerial(serial);
+		return device.save();
+	}
+	public static Long saveMac(String barcode, String mac){
+		if(barcode==null || barcode.length()<=0)return -1l;
+		if(mac==null)return -1l;
+		Device device = new Select().from(Device.class).where("Barcode = ?", barcode).executeSingle();
+		device.setBt_addr(mac);
+		return device.save();
+	}
+
+	public static boolean checkSerial(String barcode, String serial){
+		if(barcode==null || barcode.length()<=0)return false;
+		if(serial==null)return false;
+		String args[]= new String[2];
+		args[0]=barcode;
+		args[1]=serial;
+		return new Select().from(Device.class).where("Barcode = ? AND Serial = ?", args).execute().size()>0;
+	}
+
+	public static boolean checkMac(String barcode, String mac){
+		if(barcode==null || barcode.length()<=0)return false;
+		if(mac==null)return false;
+		String args[]= new String[2];
+		args[0]=barcode;
+		args[1]=mac;
+		return new Select().from(Device.class).where("Barcode = ? AND bt_addr = ?", args).execute().size()>0;
+	}
 }
