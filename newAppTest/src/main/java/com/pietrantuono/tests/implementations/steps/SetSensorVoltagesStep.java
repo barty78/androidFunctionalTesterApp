894744ed251e77a6bf1043b00e81368c3517c09d
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2016-01-22 12:30:08 +0100

    Use HandlerThread in SetSensorVoltagesStep

--- newAppTest/src/main/java/com/pietrantuono/tests/implementations/steps/SetSensorVoltagesStep.java
+++ newAppTest/src/main/java/com/pietrantuono/tests/implementations/steps/SetSensorVoltagesStep.java
@@ -2,12 +2,14 @@ package com.pietrantuono.tests.implementations.steps;
 
 import android.app.Activity;
 import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.util.Log;
 
 import com.pietrantuono.tests.implementations.BluetoothConnectTest;
 import com.pietrantuono.tests.superclass.Test;
 
-public class SetSensorVoltagesStep extends Test implements Step{
+public class SetSensorVoltagesStep extends Test implements Step {
     private short refVoltage;
     private short zeroVoltage;
 
@@ -16,8 +18,8 @@ public class SetSensorVoltagesStep extends Test implements Step{
      * {@link BluetoothConnectTest}
      * Do not execute this step before opening Bluetooth
      *
-     * @param refVoltage        , sensor gain voltage (0 to 255, -1 if not setting)
-     * @param zeroVoltage       , sensor zero offset voltage (0 to 255, -1 if not setting)
+     * @param refVoltage  , sensor gain voltage (0 to 255, -1 if not setting)
+     * @param zeroVoltage , sensor zero offset voltage (0 to 255, -1 if not setting)
      * @param activity
      * @param description
      */
@@ -30,53 +32,50 @@ public class SetSensorVoltagesStep extends Test implements Step{
 
     @Override
     public void execute() {
-        if(isinterrupted)return;
-        Log.d(TAG, "Step Executing: " + description);
-        if(getListener().getBtutility()==null){
+        Log.d(TAG, "Executing: " + description);
+        final HandlerThread handlerThread = new HandlerThread("SetSensorVoltagesStep");
+        handlerThread.start();
+        Handler handler = new Handler(handlerThread.getLooper());
+        Log.d(TAG,"Looper of the HandlerThread ="+handlerThread.getLooper().toString());
+        Log.d(TAG,"Looper of UI thread ="+ Looper.getMainLooper().toString());
+        if (isinterrupted) return;
+        Log.d(TAG, "Executing: " + description);
+        if (getListener().getBtutility() == null) {
             report("BU utility is null");
             getListener().addFailOrPass(false, false, "BT ERROR");
-            return;}
-
+            return;
+        }
         if (refVoltage != -1) {
             try {
                 getListener().getBtutility().setVoltage(refVoltage);
             } catch (Exception e) {
                 getListener().addFailOrPass(true, false, "ERROR", "UUT Comms Fault");
                 return;
-
             }
         }
-        if(isinterrupted)return;
-        Handler handler = new Handler();
-
+        if (isinterrupted) return;
         if (zeroVoltage != -1) {
             try {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
+                        Log.d(TAG, "setZeroVoltage " + zeroVoltage);
                         getListener().getBtutility().setZeroVoltage(zeroVoltage);
                     }
                 }, 100);
             } catch (Exception e) {
                 getListener().addFailOrPass(true, false, "ERROR", "UUT Comms Fault");
                 return;
-
             }
         }
-
-        if(isinterrupted)return;
-
-
+        if (isinterrupted) return;
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 getListener().getBtutility().pollSensor();
             }
         }, 1000);
-
-
-        if(isinterrupted)return;
-
+        if (isinterrupted) return;
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -84,7 +83,14 @@ public class SetSensorVoltagesStep extends Test implements Step{
             }
         }, 2000);
 
-        if(isinterrupted)return;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                handlerThread.quitSafely();
+            }
+        }, 3000);
+
+        if (isinterrupted) return;
 
     }
 
