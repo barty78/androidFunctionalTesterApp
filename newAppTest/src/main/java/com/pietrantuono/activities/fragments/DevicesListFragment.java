625af8ddfe77a4df1746bf9d87cc92ff18a0cca7
Peter <pbartlett@analyticamedical.com>
2016-01-27 13:57:24 +1000

    work on devices list fragment row layout.

--- newAppTest/src/main/java/com/pietrantuono/activities/fragments/DevicesListFragment.java
+++ newAppTest/src/main/java/com/pietrantuono/activities/fragments/DevicesListFragment.java
@@ -103,11 +103,14 @@ public class DevicesListFragment extends Fragment {
         for (int i = 0; i < temp.size(); i++) {
             Log.d("DevListFrag", String.valueOf(temp.get(i).getJobId()) + " | " + String.valueOf(job.getId()));
             // Check if we only want devices from the current job, if so check job ids match.
-            if (!thisJobOnly || (thisJobOnly && (temp.get(i).getJobId() == job.getId()))) {
+            if ((thisJobOnly && (temp.get(i).getJobId() == job.getId()))) {
                 // Only add devices which have actually executed the current test type
                 if ((temp.get(i).getExec_Tests() & job.getTesttypeId()) == job.getTesttypeId()) {
                     devices.add(temp.get(i));
                 }
+            } else {
+                // We want all devices from all jobs
+                devices.add(temp.get(i));
             }
         }
         if(devices.size()<=0)state.setViewState(MultiStateView.VIEW_STATE_EMPTY);
@@ -121,7 +124,7 @@ public class DevicesListFragment extends Fragment {
         RetrofitRestServices.getRest(getActivity()).getLastDevices(PeriCoachTestApplication.getDeviceid(), PeriCoachTestApplication.getLastId(), new Callback<DevicesList>() {
             @Override
             public void success(DevicesList arg0, Response arg1) {
-                if(swiper!=null)swiper.setRefreshing(false);
+                if (swiper != null) swiper.setRefreshing(false);
                 if (arg0 != null) ServiceDBHelper.addDevices(arg0);
                 else Snackbar.make(state, "Failed devices download", Snackbar.LENGTH_LONG).show();
                 populateList();
@@ -129,7 +132,7 @@ public class DevicesListFragment extends Fragment {
 
             @Override
             public void failure(RetrofitError arg0) {
-                if(swiper!=null)swiper.setRefreshing(false);
+                if (swiper != null) swiper.setRefreshing(false);
                 Snackbar.make(state, "Failed devices download", Snackbar.LENGTH_LONG).show();
                 populateList();
             }
