00f6987eb986a49693f8397e5c830e5aead8fe1b
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2016-02-09 08:21:56 +0100

    Added popup menu in actionmode, white text color of toolbar, switch text

--- newAppTest/src/main/java/com/pietrantuono/fragments/devices/ActionModecallback.java
+++ newAppTest/src/main/java/com/pietrantuono/fragments/devices/ActionModecallback.java
@@ -1,18 +1,23 @@
 package com.pietrantuono.fragments.devices;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.view.ActionMode;
+import android.support.v7.widget.PopupMenu;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.pietrantuono.pericoach.newtestapp.R;
 
@@ -31,32 +36,24 @@ public class ActionModecallback implements ActionMode.Callback {
     }
 
     @Override
-    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+    public boolean onCreateActionMode(final ActionMode mode, final Menu menu) {
         MenuInflater inflater = mode.getMenuInflater();
         inflater.inflate(R.menu.context_menu, menu);
-        Spinner spinner = (Spinner) MenuItemCompat.getActionView(menu.findItem(R.id.spinner));
-        ArrayAdapter<CharSequence> adapter =  new ArrayAdapter<CharSequence>(context,android.R.layout.simple_spinner_item, new String[]{"Sort by result","Sort by barcode"});
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.d(TAG, "Selected " + position);
-                if(position==0)callback.sortByResult();
-                if(position==1)callback.sortByBarcode();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-        Switch aSwitch= (Switch) MenuItemCompat.getActionView(menu.findItem(R.id.currentjobonly));
+        final Switch aSwitch = (Switch) MenuItemCompat.getActionView(menu.findItem(R.id.currentjobonly));
+        aSwitch.setText("Current job only");
         aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked)callback.currentJobOnly(true);
-                else callback.currentJobOnly(false);
+                if (isChecked) {
+                    callback.currentJobOnly(true);
+                    aSwitch.setText("Current job only");
+                    mode.invalidate();
+                }
+                else {
+                    callback.currentJobOnly(false);
+                    aSwitch.setText("All jobs");
+                    mode.invalidate();
+                }
             }
         });
 
@@ -72,7 +69,31 @@ public class ActionModecallback implements ActionMode.Callback {
 
     @Override
     public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        if (item.getItemId() == R.id.click) {
+            showPopup(item);
+            return true;
+        }
         return false;
+    }
+
+    private void showPopup(MenuItem item) {
+        PopupMenu popup = new PopupMenu(context, ((Activity)context).findViewById(item.getItemId()));
+        popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.sort_by_barcode:
+                        callback.sortByBarcode();
+                        return true;
+                    case R.id.sort_by_result:
+                        callback.sortByResult();
+                        return true;
+                }
+                return true;
+            }
+        });
+        popup.show();
     }
 
     @Override
